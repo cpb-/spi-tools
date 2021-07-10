18f6b238a088c2206cf44fc2d7fe2f09ab3ccb65
Christophe Blaess <christophe.blaess@logilin.fr>
2021-07-10 18:33:52 +0200

    [DEV] Support SPI mode option in `spi-pipe`

--- src/spi-pipe.c
+++ src/spi-pipe.c
@@ -37,7 +37,12 @@ static void display_usage(const char *name)
 {
 	fprintf(stderr, "usage: %s options...\n", name);
 	fprintf(stderr, "  options:\n");
-	fprintf(stderr, "    -d --device=<dev>    use the given spi-dev character device.\n");
+	fprintf(stderr, "    -d --device=<dev>    Use the given spi-dev character device.\n");
+	fprintf(stderr, "    -m --mode=[0-3]      Use the selected spi mode:\n");
+	fprintf(stderr, "             0: low idle level, sample on leading edge,\n");
+	fprintf(stderr, "             1: low idle level, sample on trailing edge,\n");
+	fprintf(stderr, "             2: high idle level, sample on leading edge,\n");
+	fprintf(stderr, "             3: high idle level, sample on trailing edge.\n");
 	fprintf(stderr, "    -s --speed=<speed>   Maximum SPI clock rate (in Hz).\n");
 	fprintf(stderr, "    -B --bits=[7...]     Bits per word.\n");
 	fprintf(stderr, "    -b --blocksize=<int> transfer block size in byte.\n");
@@ -56,6 +61,7 @@ int main (int argc, char *argv[])
 		{"speed",     required_argument, NULL,  's' },
 		{"blocksize", required_argument, NULL,  'b' },
 		{"number",    required_argument, NULL,  'n' },
+		{"mode",      required_argument, NULL,  'm' },
 		{"bits",      required_argument, NULL,  'B' },
 		{"help",      no_argument,       NULL,  'h' },
 		{"version",   no_argument,       NULL,  'v' },
@@ -66,13 +72,14 @@ int main (int argc, char *argv[])
 	char *device = NULL;
 	uint8_t *rx_buffer = NULL;
 	uint8_t *tx_buffer = NULL;
-	int blocksize   =  1;
-	int blocknumber = -1;
-	int bitsperword = -1;
-	int offset      =  0;
-	int nb          =  0;
-	int speed       = -1;
-	int orig_speed  = -1;
+	int spi_mode      = -1;
+	int block_size    =  1;
+	int blocks_count  = -1;
+	int bits_per_word = -1;
+	int offset        =  0;
+	int nb            =  0;
+	int speed         = -1;
+	int orig_speed    = -1;
 
 	struct spi_ioc_transfer transfer = {
 		.tx_buf        = 0,
@@ -83,7 +90,7 @@ int main (int argc, char *argv[])
 		.bits_per_word = 0,
 	};
 
-	while ((opt = getopt_long(argc, argv, "d:s:b:n:B:rhv", options, &long_index)) >= 0) {
+	while ((opt = getopt_long(argc, argv, "d:s:b:m:n:B:rhv", options, &long_index)) >= 0) {
 		switch(opt) {
 			case 'h':
 				display_usage(argv[0]);
@@ -97,9 +104,17 @@ int main (int argc, char *argv[])
 				device = optarg;
 				break;
 
+			case 'm':
+				if ((sscanf(optarg, "%d", &spi_mode) != 1)
+				 || (spi_mode < 0) || (spi_mode > 3)) {
+					fprintf(stderr, "%s: wrong SPI mode ([0-3])\n", argv[0]);
+					exit(EXIT_FAILURE);
+				}
+				break;
+
 			case 'b':
-				if ((sscanf(optarg, "%d", &blocksize) != 1)
-				 || (blocksize <= 0)) {
+				if ((sscanf(optarg, "%d", &block_size) != 1)
+				 || (block_size <= 0)) {
 					fprintf(stderr, "%s: wrong blocksize\n", argv[0]);
 					exit(EXIT_FAILURE);
 				}
@@ -114,16 +129,16 @@ int main (int argc, char *argv[])
 				break;
 
 			case 'B':
-				if ((sscanf(optarg, "%d", &bitsperword) != 1)
-				 || (bitsperword < 7)) {
+				if ((sscanf(optarg, "%d", &bits_per_word) != 1)
+				 || (bits_per_word < 7)) {
 					fprintf(stderr, "%s: wrong bits per word value [7...]\n", argv[0]);
 					exit(EXIT_FAILURE);
 				}
 				break;
 
 			case 'n':
-				if ((sscanf(optarg, "%d", &blocknumber) != 1)
-				 || (blocknumber < -1)) {
+				if ((sscanf(optarg, "%d", &blocks_count) != 1)
+				 || (blocks_count < -1)) {
 					fprintf(stderr, "%s: wrong block number\n", argv[0]);
 					exit(EXIT_FAILURE);
 				}
@@ -135,19 +150,19 @@ int main (int argc, char *argv[])
 		}
 	}
 
-	if (((rx_buffer = malloc(blocksize)) == NULL)
-	 || ((tx_buffer = malloc(blocksize)) == NULL)) {
+	if (((rx_buffer = malloc(block_size)) == NULL)
+	 || ((tx_buffer = malloc(block_size)) == NULL)) {
 		fprintf(stderr, "%s: not enough memory to allocate two %d bytes buffers\n",
-		                argv[0], blocksize);
+		                argv[0], block_size);
 		exit(EXIT_FAILURE);
 	}
 
-	memset(rx_buffer, 0, blocksize);
-	memset(tx_buffer, 0, blocksize);
+	memset(rx_buffer, 0, block_size);
+	memset(tx_buffer, 0, block_size);
 
 	transfer.rx_buf = (unsigned long)rx_buffer;
 	transfer.tx_buf = (unsigned long)tx_buffer;
-	transfer.len = blocksize;
+	transfer.len = block_size;
 
 	if (device == NULL) {
 		fprintf(stderr, "%s: no device specified (use option -h for help).\n", argv[0]);
@@ -172,17 +187,24 @@ int main (int argc, char *argv[])
 		}
 	}
 
-	if (bitsperword != -1) {
-		if (ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bitsperword) < 0) {
-			fprintf(stderr, "Unable to set bits to %d\n", bitsperword);
+	if (spi_mode != -1) {
+		if (ioctl(fd, SPI_IOC_WR_MODE, &spi_mode) < 0) {
+			perror("SPI_IOC_WR_MODE");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (bits_per_word != -1) {
+		if (ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits_per_word) < 0) {
+			fprintf(stderr, "Unable to set bits to %d\n", bits_per_word);
 			perror("SPI_IOC_WR_BITS_PER_WORD");
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	while ((blocknumber > 0) || (blocknumber == -1)) {
-		for (offset = 0; offset < blocksize; offset += nb) {
-			nb = read(STDIN_FILENO, & (tx_buffer[offset]), blocksize - offset);
+	while ((blocks_count > 0) || (blocks_count == -1)) {
+		for (offset = 0; offset < block_size; offset += nb) {
+			nb = read(STDIN_FILENO, & (tx_buffer[offset]), block_size - offset);
 			if (nb <= 0)
 				break;
 		}
@@ -196,8 +218,8 @@ int main (int argc, char *argv[])
 		}
 		if (write(STDOUT_FILENO, rx_buffer, offset) <= 0)
 			break;
-		if (blocknumber > 0)
-			blocknumber --;
+		if (blocks_count > 0)
+			blocks_count --;
 	}
 
 	if (orig_speed != -1) {
@@ -210,7 +232,7 @@ int main (int argc, char *argv[])
 	free(rx_buffer);
 	free(tx_buffer);
 
-	if (blocknumber != 0)
+	if (blocks_count != 0)
 		return EXIT_FAILURE;
 
 	return EXIT_SUCCESS;
